d47f64c780634182efdeea61b2022344bae3279d
Lucas <lupelikan@int.pl>
2025-07-13 15:12:33 +0200

    Made microservices eureka clients and tried making multiple instances of service

--- microservices/user-service/src/main/java/com/karate/userservice/infrastructure/client/KarateClubClient.java
+++ microservices/user-service/src/main/java/com/karate/userservice/infrastructure/client/KarateClubClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "club-service", url = "${club-service.url}")
+@FeignClient(name = "club-service")
 public interface KarateClubClient {
     @GetMapping("/clubs/by-name")
     KarateClubDto getClubByName(@RequestParam("name") String clubName);
