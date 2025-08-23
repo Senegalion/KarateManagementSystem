3ba45cdd40dc4d30479ae77f7abdccbf1270d621
Lucas <lupelikan@int.pl>
2025-08-23 09:19:04 +0200

    Created feedback microservice

--- microservices/feedback-service/src/main/java/com/karate/feedback_service/FeedbackServiceApplication.java
+++ microservices/feedback-service/src/main/java/com/karate/feedback_service/FeedbackServiceApplication.java
@@ -1,11 +1,14 @@
 package com.karate.feedback_service;
 
+import com.karate.feedback_service.infrastructure.jwt.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class FeedbackServiceApplication {
 
     public static void main(String[] args) {
