0379fc8f83922f75c8efa422c6c17195219080b3
Łukasz Pelikan <lupelikan@gmail.com>
2026-01-13 10:32:02 +0100

    create system admin role

--- microservices/auth-service/src/main/java/com/karate/authservice/infrastructure/client/UserClient.java
+++ microservices/auth-service/src/main/java/com/karate/authservice/infrastructure/client/UserClient.java
@@ -4,10 +4,7 @@ import com.karate.authservice.infrastructure.client.config.FeignClientConfig;
 import com.karate.authservice.infrastructure.client.dto.NewUserRequestDto;
 import com.karate.authservice.infrastructure.client.dto.UserInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "user-service", configuration = FeignClientConfig.class)
 public interface UserClient {
@@ -16,4 +13,8 @@ public interface UserClient {
 
     @GetMapping("/internal/users/{id}")
     UserInfoDto getUserById(@PathVariable("id") Long userId);
+
+    @PutMapping("/internal/users/{userId}/club/{clubId}")
+    void updateUserClubId(@PathVariable("userId") Long userId,
+                          @PathVariable("clubId") Long clubId);
 }
